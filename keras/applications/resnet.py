a6801228eb291b822889525c311ca36556768aea
Sarvagya <sarvagya@blunav.in>
2021-09-20 18:41:56 +0530

    Adding ResNet

--- keras/applications/resnet.py
+++ keras/applications/resnet.py
@@ -30,10 +30,14 @@ from keras.utils import data_utils
 from keras.utils import layer_utils
 from tensorflow.python.util.tf_export import keras_export
 
-
+# TODO - add weights for imagenet 
 BASE_WEIGHTS_PATH = (
     'https://storage.googleapis.com/tensorflow/keras-applications/resnet/')
 WEIGHTS_HASHES = {
+    'resnet18': ('',
+                 ''),
+    'resnet34': ('',
+                 ''),
     'resnet50': ('2cb95161c43110f7111970584f804107',
                  '4d473c1dd8becc155b73f8504c6f6626'),
     'resnet101': ('f1aeb4b969a6efcfb50fad2f0c20cfc5',
@@ -208,6 +212,62 @@ def ResNet(stack_fn,
     model.load_weights(weights)
 
   return model
+
+def block0(x, filters, kernel_size=3, stride=1, conv_shortcut=True, name=None):
+  """A residual block.
+
+  Args:
+    x: input tensor.
+    filters: integer, filters of the bottleneck layer.
+    kernel_size: default 3, kernel size of the bottleneck layer.
+    stride: default 1, stride of the first layer.
+    conv_shortcut: default True, use convolution shortcut if True,
+        otherwise identity shortcut.
+    name: string, block label.
+
+  Returns:
+    Output tensor for the residual block.
+  """
+  bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
+
+  if conv_shortcut:
+    shortcut = layers.Conv2D(
+        filters, 1, strides=stride, name=name + '_0_conv')(x)
+    shortcut = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name=name + '_0_bn')(shortcut)
+  else:
+    shortcut = x
+
+  x = layers.Conv2D(
+      filters, kernel_size, padding='SAME', strides=stride, name=name + '_1_conv')(x)
+
+  x = layers.BatchNormalization(
+      axis=bn_axis, epsilon=1.001e-5, name=name + '_1_bn')(x)
+  x = layers.Activation('relu', name=name + '_1_relu')(x)
+  x = layers.Conv2D(filters, kernel_size, padding='SAME', name=name + '_2_conv')(x)
+
+  x = layers.Add(name=name + '_add')([shortcut, x])
+  x = layers.Activation('relu', name=name + '_out')(x)
+  return x
+
+
+def stack0(x, filters, blocks, stride1=2, name=None):
+  """A set of stacked residual blocks.
+
+  Args:
+    x: input tensor.
+    filters: integer, filters of the bottleneck layer in a block.
+    blocks: integer, blocks in the stacked blocks.
+    stride1: default 2, stride of the first layer in the first block.
+    name: string, stack label.
+
+  Returns:
+    Output tensor for the stacked blocks.
+  """
+  x = block0(x, filters, stride=stride1, name=name + '_block1')
+  for i in range(2, blocks + 1):
+    x = block0(x, filters, conv_shortcut=False, name=name + '_block' + str(i))
+  return x
 
 
 def block1(x, filters, kernel_size=3, stride=1, conv_shortcut=True, name=None):
@@ -437,6 +497,50 @@ def stack3(x, filters, blocks, stride1=2, groups=32, name=None):
   return x
 
 
+@keras_export('keras.applications.resnet18.ResNet18',
+              'keras.applications.resnet.ResNet18',
+              'keras.applications.ResNet18')
+def ResNet18(include_top=True,
+             weights='imagenet',
+             input_tensor=None,
+             input_shape=None,
+             pooling=None,
+             classes=1000,
+             **kwargs):
+  """Instantiates the ResNet18 architecture."""
+
+  def stack_fn(x):
+    x = stack0(x, 64, 2, stride1=1, name='conv2')
+    x = stack0(x, 128, 2, name='conv3')
+    x = stack0(x, 256, 2, name='conv4')
+    return stack0(x, 512, 2, name='conv5')
+
+  return ResNet(stack_fn, False, True, 'resnet18', include_top, weights,
+                input_tensor, input_shape, pooling, classes, **kwargs)
+
+
+@keras_export('keras.applications.resnet34.ResNet34',
+              'keras.applications.resnet.ResNet34',
+              'keras.applications.ResNet34')
+def ResNet34(include_top=True,
+             weights='imagenet',
+             input_tensor=None,
+             input_shape=None,
+             pooling=None,
+             classes=1000,
+             **kwargs):
+  """Instantiates the ResNet34 architecture."""
+
+  def stack_fn(x):
+    x = stack0(x, 64, 3, stride1=1, name='conv2')
+    x = stack0(x, 128, 4, name='conv3')
+    x = stack0(x, 256, 6, name='conv4')
+    return stack0(x, 512, 3, name='conv5')
+
+  return ResNet(stack_fn, False, True, 'resnet34', include_top, weights,
+                input_tensor, input_shape, pooling, classes, **kwargs)
+
+
 @keras_export('keras.applications.resnet50.ResNet50',
               'keras.applications.resnet.ResNet50',
               'keras.applications.ResNet50')
@@ -580,6 +684,8 @@ DOC = """
     A Keras model instance.
 """
 
+setattr(ResNet18, '__doc__', ResNet18.__doc__ + DOC)
+setattr(ResNet34, '__doc__', ResNet34.__doc__ + DOC)
 setattr(ResNet50, '__doc__', ResNet50.__doc__ + DOC)
 setattr(ResNet101, '__doc__', ResNet101.__doc__ + DOC)
 setattr(ResNet152, '__doc__', ResNet152.__doc__ + DOC)
